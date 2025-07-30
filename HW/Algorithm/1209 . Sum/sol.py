9a0c3ec23d3498942108b5357514cc7bb971f14d
최준형 <chl984@naver.com>
2025-07-31 00:02:26 +0900

    함정에 당한 날

--- HW/Algorithm/1209 . Sum/sol.py
+++ HW/Algorithm/1209 . Sum/sol.py
@@ -1,12 +1,13 @@
 import sys
 # open을 사용해서 input 파일을 연다
-sys.stdin = open('sample_input.txt')
+sys.stdin = open('input.txt')
+# sys.setrecursionlimit(10000)
 
 for _ in range(10): # 테스트케이스 수
     tc = input() # 테스트케이스 번호 입력
     # 입력 받은 문자열을 공백 기준으로 쪼개서 정수로 바꾼다음 리스트에 담는걸 100번반복
-    data = [list(map(int, input().split()) for _ in range(100))]
-    print(data)
+    data = [list(map(int, input().split())) for _ in range(100)]
+    # print(data)
     # 각 행마다 가진 값들을 더한다.
     # 각 열마다 가진 값들을 더한다.
     # 대각선의 값들을 더한다.
@@ -24,3 +25,79 @@ for _ in range(10): # 테스트케이스 수
     #     # data.append(tmp_list)   # 그 리스트를 data에 추가
     #     data.append(map_to_list_data)
     # print(data)
+
+    def row_max(row_idx, mx):
+        if row_idx >= len(data):
+            return mx
+        sm = row_sum(row_idx, 0, 0)
+        if sm > mx:
+            mx = sm
+        return row_max(row_idx + 1, mx)
+
+    def row_sum(x, y, sm):
+        if y >= len(data[0]):
+            return sm
+        return row_sum(x, y + 1, sm + data[x][y])
+
+    def col_max(col_idx, mx):
+        if col_idx >= len(data[0]):
+            return mx
+        sm = col_sum(0, col_idx, 0)
+        if sm > mx:
+            mx = sm
+        return col_max(col_idx + 1, mx)
+
+    def col_sum(x, y, sm):
+        if x >= len(data):
+            return sm
+        return col_sum(x + 1, y, sm + data[x][y])
+
+    def down_max(start_row, mx):
+        if start_row >= len(data):
+            return mx
+        sm = down_sum(start_row, 0, 0)
+        if sm > mx:
+            mx = sm
+        return down_max(start_row + 1, mx)
+
+    def down_sum(x, y, sm):
+        if x >= len(data) or y >= len(data[0]):
+            return sm
+        return down_sum(x + 1, y + 1, sm + data[x][y])
+
+    def up_max(start_row, mx):
+        if start_row < 0:
+            return mx
+        sm = up_sum(start_row, 0, 0)
+        if sm > mx:
+            mx = sm
+        return up_max(start_row - 1, mx)
+
+    def up_sum(x, y, sm):
+        if x < 0 or y >= len(data[0]):
+            return sm
+        return up_sum(x - 1, y + 1, sm + data[x][y])
+
+    def data_max():
+        r = row_max(0, 0)
+        c = col_max(0, 0)
+        d1 = down_max(0, 0)
+        d2 = up_max(len(data) - 1, 0)
+
+        if r > c:
+            bigger1 = r
+        else:
+            bigger1 = c
+
+        if d1 > d2:
+            bigger2 = d1
+        else:
+            bigger2 = d2
+
+        if bigger1 > bigger2:
+            return bigger1
+        else:
+            return bigger2
+
+    print(f"#{tc} {data_max()}")
+
