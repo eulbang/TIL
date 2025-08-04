9c6256f6204947e5a38de9d10737555b717b4fb9
최준형 <chl984@naver.com>
2025-08-04 20:57:44 +0900

    homework

--- HW/Algorithm/2382. 미생물 격리/sol.py
+++ HW/Algorithm/2382. 미생물 격리/sol.py
@@ -18,6 +18,68 @@ for test_case in range(1, T + 1):
             for y in range(N):
                 if area[x][y] is None:
                     continue
+                count, direction = area[x][y]
+                if direction == 1:
+                    nx, ny = x - 1, y
+                elif direction == 2:
+                    nx, ny = x + 1, y
+                elif direction == 3:
+                    nx, ny = x, y - 1
+                else:
+                    nx, ny = x, y + 1
+                if nx == 0 or ny == 0 or nx == N - 1 or ny == N - 1:
+                    count //= 2
+                    if count == 0:
+                        continue
+                    if direction == 1:
+                        ndir = 2
+                    elif direction == 2:
+                        ndir = 1
+                    elif direction == 3:
+                        ndir = 4
+                    else:
+                        ndir = 3
+                else:
+                    ndir = direction
+                if future[nx][ny] is not None:
+                    if future[nx][ny][2] < count:
+                        future[nx][ny][1] = ndir
+                        future[nx][ny][2] = count
+                    future[nx][ny][0] += count
+                else:
+                    future[nx][ny] = [count, ndir, count]
+        for x in range(N):
+            for y in range(N):
+                if future[x][y] is not None:
+                    future[x][y] = [future[x][y][0], future[x][y][1]]
+        area = future
+
+    total = 0
+    for i in range(N):
+        for j in range(N):
+            if area[i][j] is not None:
+                total += area[i][j][0]
+
+    print(f"#{test_case} {total}")
+
+'''
+T = int(input())
+for test_case in range(1, T + 1):
+    N, M, K = map(int, input().split())
+
+    area = [[None] * N for _ in range(N)]
+
+    for _ in range(K):
+        r, c, count, direct = map(int, input().split())
+        area[r][c] = [count, direct]
+
+    for _ in range(M):
+        future = [[None] * N for _ in range(N)]
+
+        for x in range(N):
+            for y in range(N):
+                if area[x][y] is None:
+                    continue
                 direction = area[x][y][1]
 
                 if direction == 1:  # 위: (x-1, y)
@@ -59,4 +121,4 @@ for test_case in range(1, T + 1):
                 total += area[i][j][0]
 
     print(f"#{test_case} {total}")
-
+'''
