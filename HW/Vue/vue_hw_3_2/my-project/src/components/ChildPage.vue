df256bc4ea79ec05911e9c5d1182c802860a9418
최준형 <chl984@naver.com>
2025-09-03 21:01:56 +0900

    rhkwp

---
+++ HW/Vue/vue_hw_3_2/my-project/src/components/ChildPage.vue
@@ -0,0 +1,17 @@
+<!-- src/components/ChildPage.vue -->
+<template>
+  <div>
+    <h3>자식 페이지입니다.</h3>
+    <p>이름: {{ child.name }}</p>
+    <p>나이: {{ child.age }}</p>
+  </div>
+</template>
+
+<script setup>
+const { child } = defineProps({
+  child: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
