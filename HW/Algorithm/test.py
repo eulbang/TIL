aac4140b42e39d0a3af17b1c6d835454d4f215b0
eulbang <chl984@naver.com>
2025-09-05 17:42:34 +0900

    tndjq

--- HW/Algorithm/test.py
+++ HW/Algorithm/test.py
@@ -1,35 +1,13 @@
-def dfs(diff, nums, last_num, res):
-    global mx_num, mn_num
+N, M, K = map(int, input().split())
+nums = []
+for _ in range(N):
+    nums.append(int(input()))
+control = []
+for _ in range(M+K):
+    control.append(list(map(int, input().split())))
 
-    if len(res) == k + 1:
-        num = 0
-        for d in res:
-            num = num*10 + d
-        mx_num = max(mx_num, num)
-        mn_num = min(mn_num, num)
-        return
-
-    sign = diff[len(res)-1]
-    if sign == '<':
-        for i in nums:
-            if i > last_num:
-                next_nums = [x for x in nums if x != i]
-                dfs(diff, next_nums, i, res+[i])
+for a, b, c in control:
+    if a == 1:
+        nums[b-1] = c
     else:
-        for i in nums:
-            if i < last_num:
-                next_nums = [x for x in nums if x != i]
-                dfs(diff, next_nums, i, res+[i])
-
-
-k = int(input())
-diff = input().split()
-nums = list(range(10))
-mx_num = -1
-mn_num = 10**(k + 1)
-
-for s in range(10):
-    dfs(diff, [x for x in nums if x != s], s, [s])
-
-print(str(mx_num).zfill(k + 1))
-print(str(mn_num).zfill(k + 1))
+        print(sum(nums[b-1:c]))
