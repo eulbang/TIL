61e9ee29eaf512f808664d083145d76f658e5f71
eulbang <chl984@naver.com>
2025-09-17 17:51:41 +0900

    tndjq

--- HW/Algorithm/test_sol.py
+++ HW/Algorithm/test_sol.py
@@ -1,6 +1,18 @@
-N, M, x, y, k = map(int, input().split())
-area = [list(map(int, input().split())) for _ in range(N)]
-move = list(map(int, input().split()))
+N = int(input())
 
-print(area)
-print(move)
+if N == 1:
+    print(9)
+else:
+    prev = [0]*10
+    for d in range(1, 10):
+        prev[d] = 1
+
+    for _ in range(2, N+1):
+        cur = [0]*10
+        cur[0] = prev[1] % 1000000000
+        cur[9] = prev[8] % 1000000000
+        for d in range(1, 9):
+            cur[d] = (prev[d-1] + prev[d+1]) % 1000000000
+        prev = cur
+
+    print(sum(prev) % 1000000000)
