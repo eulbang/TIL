0fe7973b3ed3df6cc0fd7cdb685b475ab7105416
eulbang <chl984@naver.com>
2025-09-26 17:52:48 +0900

    실습 제출

--- HW/Algorithm/test_sol.py
+++ HW/Algorithm/test_sol.py
@@ -1,39 +1,9 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-nums = [int(input()) for _ in range(N)]
-nums.sort()
+area = [[0]*N for _ in range(N)]
+K = int(input())
+for _ in range(K):
+    y, x = map(int, input().split())
+    area[y-1][x-1] = 1
 
-res = 0
-i = 0
-j = N - 1
-
-while i + 1 <= j and nums[i] < 1:
-    if nums[i] < 0 and nums[i+1] < 0:
-        res += nums[i] * nums[i+1]
-        i += 2
-    elif nums[i] < 0 and nums[i+1] == 0:
-        i += 2
-    else:
-        break
-
-if i <= j and nums[i] < 0:
-    res += nums[i]
-    i += 1
-
-while i <= j and nums[i] == 0:
-    i += 1
-
-while i <= j and nums[i] == 1:
-    res += 1
-    i += 1
-
-while i + 1 <= j:
-    res += nums[j] * nums[j-1]
-    j -= 2
-
-if i == j:
-    res += nums[i]
-
-print(res)
+for i in range(N):
+    print(area[i])
