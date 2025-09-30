eb9fe0b65d18c855224c615732f9d019d7722a08
eulbang <chl984@naver.com>
2025-09-30 17:58:20 +0900

    tndjq

--- HW/Algorithm/test_sol.py
+++ HW/Algorithm/test_sol.py
@@ -1,26 +1,54 @@
 from collections import deque
+from itertools import combinations
 import sys
 input = sys.stdin.readline
 
-def dfs(area, cnt):
-    global N, M
-
-    if cnt >= 3:
-        for i in range(N):
-            for j in range(M):
-                if area[i][j] == 2:
-                    pass
-        return
-
-    for y in range(N):
-        for x in range(M):
-            if area[y][x] == 0:
-                n_area = [area[i][:] for i in range(N)]
-                n_area[y][x] = 1
-                dfs(n_area, cnt+1)
-
+DY = (-1, 1, 0, 0)
+DX = (0, 0, -1, 1)
 
 N, M = map(int, input().split())
-area = [list(map(int, input().split())) for _ in range(N)]
+board = [list(map(int, input().split())) for _ in range(N)]
 
-dfs(area, 0)
+empties = []
+viruses = []
+for y in range(N):
+    row = board[y]
+    for x in range(M):
+        v = row[x]
+        if v == 0:
+            empties.append((y, x))
+        elif v == 2:
+            viruses.append((y, x))
+
+E = len(empties)
+initial_safe = E
+best = 0
+
+for walls in combinations(empties, 3):
+    temp = [row[:] for row in board]
+
+    for (wy, wx) in walls:
+        temp[wy][wx] = 1
+
+    q = deque(viruses)
+    infected = 0
+
+    limit = initial_safe - 3
+
+    while q:
+        y, x = q.popleft()
+        for k in range(4):
+            ny, nx = y + DY[k], x + DX[k]
+            if 0 <= ny < N and 0 <= nx < M and temp[ny][nx] == 0:
+                temp[ny][nx] = 2
+                infected += 1
+                if limit - infected <= best:
+                    q.clear()
+                    break
+                q.append((ny, nx))
+
+    safe = limit - infected
+    if safe > best:
+        best = safe
+
+print(best)
